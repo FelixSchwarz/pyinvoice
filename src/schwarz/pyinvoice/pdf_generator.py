1093a25553dccfd70f17df5e4e80a20275756886
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2022-12-14 23:17:21 +0100

    pyinvoice: ability to generate PDFs using custom translations

--- src/schwarz/pyinvoice/pdf_generator.py
+++ src/schwarz/pyinvoice/pdf_generator.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import subprocess
 from tempfile import NamedTemporaryFile
 
-from babel.support import Format
+from babel.support import Format, Translations
 import jinja2
 
 
@@ -13,7 +13,7 @@ __all__ = ['generate_pdf']
 def generate_pdf(invoice, invoice_cfg, target_path, *, with_logo=False):
     template_path = Path(invoice_cfg['template'])
     template_dir = template_path.parent
-    template = load_invoice_template(template_path)
+    template = load_invoice_template(template_path, locale=invoice.language)
     _format = Format(locale=invoice.language)
     _format.amount = lambda v: _format.currency(v, currency=invoice.currency)
     template_params = {'f': _format, **invoice_cfg, 'with_logo': with_logo}
@@ -23,15 +23,20 @@ def generate_pdf(invoice, invoice_cfg, target_path, *, with_logo=False):
         cmd = ['weasyprint', str(path_html.absolute()), str(target_path.absolute())]
         subprocess.run(cmd, shell=False, cwd=template_dir)
 
-def load_invoice_template(template_path):
+def load_invoice_template(template_path, locale=None):
     tmpl_loader = jinja2.FileSystemLoader(searchpath=template_path.parent)
     tmpl_env = jinja2.Environment(
         loader=tmpl_loader,
+        extensions=['jinja2.ext.i18n'],
         trim_blocks=True,
         lstrip_blocks=True,
         autoescape=True,
         undefined=jinja2.StrictUndefined,
     )
+    if locale:
+        locale_dir = (template_path.parent / 'i18n')
+        translations = Translations.load(dirname=locale_dir, locales=[locale], domain='pyinvoice')
+        tmpl_env.install_gettext_translations(translations)
     return tmpl_env.get_template(template_path.name)
 
 @contextmanager
